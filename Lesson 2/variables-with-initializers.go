8300fc29a36129fabe765be6f88fbfb4eef0e733
Eddie Hsu <aoe102198@gmail.com>
2024-08-08 22:38:54 +0800

    Add variables-with-initializers.go

---
+++ Lesson 2/variables-with-initializers.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Similar with typescript.
+var i, j int = 1, 2
+
+func main() {
+	// Initial the variable in list declaration.
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
+}
