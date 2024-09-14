2481d95b6dea77833c2e967accae2423cb6c5b88
Eddie Hsu <aoe102198@gmail.com>
2024-09-14 23:52:16 +0800

    Complete range-continued.go

---
+++ Lesson 4/range-continued.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	pow := make([]int, 10)
+	// Default [0 0 0 0 0 0 0 0 0 0]
+	fmt.Println(pow)
+
+	for i := range pow {
+		// Shift 1 left i unit
+		pow[i] = 1 << uint(i)
+	}
+
+	// You can skip the index or value by assigning to _.
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
+}
