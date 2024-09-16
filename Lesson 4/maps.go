0af3815f6687f5d2814d5bd48819cd4e2a10e861
Eddie Hsu <aoe102198@gmail.com>
2024-09-17 01:02:36 +0800

    Add more instructions

--- Lesson 4/maps.go
+++ Lesson 4/maps.go
@@ -2,15 +2,18 @@ package main
 
 import "fmt"
 
-
+// A type of struct is a collection of fields.
 type Vertex struct {
 	Lat, Long float64
 }
 
+// "map" is a key, value data type.
+// String key and Vertex value
 var m map[string]Vertex
 
 func main() {
 	m = make(map[string]Vertex)
+	// Input float64 constructor into Vertex struct type
 	m["Bell Labs"] = Vertex{
 		40.68433, -74.39967,
 	}
