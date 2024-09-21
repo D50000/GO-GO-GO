d91dacb98f6a1dd35c4a390ffe46fff1ee0357ec
Eddie Hsu <aoe102198@gmail.com>
2024-09-21 23:52:45 +0800

    Add exercise-maps.go

---
+++ Lesson 4/exercise-maps.go
@@ -0,0 +1,14 @@
+package main
+
+// TODO: Fix no required module provides package golang.org/x/tour/wc
+import (
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	return map[string]int{"x": 1}
+}
+
+func main() {
+	wc.Test(WordCount)
+}
