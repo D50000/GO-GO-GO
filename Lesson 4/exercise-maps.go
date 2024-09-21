3b939cb899d7b89bead1cd1c4e53a13ed3791e22
Eddie Hsu <aoe102198@gmail.com>
2024-09-22 01:14:27 +0800

    Complete more instructions for map manipulate

--- Lesson 4/exercise-maps.go
+++ Lesson 4/exercise-maps.go
@@ -1,5 +1,7 @@
 package main
 
+// To Fix no required module provides package golang.org/x/tour/wc
+// `go install` and it will install module at: `$GOPATH/pkg/mod`
 import (
 	"fmt"
 	"strings"
@@ -8,13 +10,15 @@ import (
 )
 
 func WordCount(s string) map[string]int {
-	var words []string = strings.Split(s, " ")
+	var words []string = strings.Split(s, " ") // strings.Split()
 	fmt.Println(words)
 
 	var result = make(map[string]int)
+	// `_` for ignore the index parameter in range method.
 	for _, v := range words {
-		_, isExists := result[v];
-		if isExists {
+		_, isExists := result[v]
+		// Ignore the "value" return in map["key"]
+		if isExists { // Shorthand for => if _, isExists := result[v]; isExists { }
 			result[v] += 1
 		} else {
 			result[v] = 1
@@ -25,5 +29,7 @@ func WordCount(s string) map[string]int {
 
 func main() {
 	fmt.Println("Test run:")
+
+	// golang lib for test suit for the method.
 	wc.Test(WordCount)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/m/v2
+
+go 1.22.5
+// Dependencies for modules at: $GOPATH/pkg/mod
+require golang.org/x/tour v0.1.0 // indirect
