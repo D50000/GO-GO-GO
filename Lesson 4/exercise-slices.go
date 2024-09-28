fe9f70b91821d2f79dbaab6775a7cd0b2795d19c
Eddie Hsu <aoe102198@gmail.com>
2024-09-28 19:47:29 +0800

    Complete exercise-slices.go

--- Lesson 4/exercise-slices.go
+++ Lesson 4/exercise-slices.go
@@ -2,13 +2,21 @@ package main
 
 // Use `go list -m golang.org/x/tour` to check the package install or not.
 import (
-	"fmt"
-
 	"golang.org/x/tour/pic"
 )
 
+// When you run the program, it will display your picture, interpreting the integers as grayscale (well, bluescale) values.
 func Pic(dx, dy int) [][]uint8 {
 	// code here
+	image := make([][]uint8, dy) // Declare 2D slices.
+	for y := 0; y < dy; y++ {
+        row := make([]uint8, dx) // Declare dx slices, set dx into axis y.
+        for x := 0; x < dx; x++ {
+            row[x] = uint8(x ^ y) // To calculate the pixel value x ^ y
+        }
+        image[y] = row
+    }
+    return image
 }
 
 func main() {
