af06be93060c13f1c4663576ef5e122e89e9c5f6
Eddie Hsu <aoe102198@gmail.com>
2024-10-04 21:23:12 +0800

    Complete the exercise-fibonacci-closure.go
    - Recursive application
    - Closure concept

--- Lesson 4/exercise-fibonacci-closure.go
+++ Lesson 4/exercise-fibonacci-closure.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
-// fibonacci is a function that returns
-// a function that returns an int.
+// fibonacci is a function that returns a function that returns an int.
 func fibonacci() func() int {
 	// code here
+	// Implement a fibonacci function that returns a function (a closure) that returns successive fibonacci numbers (0, 1, 1, 2, 3, 5, ...).
+	var a, b int = 0, 1
+	// Initials
+	return func() int {
+		var temp int = a
+		a = b
+		b = temp + b
+		return temp
+	}
 }
 
 func main() {
