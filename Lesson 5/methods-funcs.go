ac7a1bdab72d788669cf8d3d6ea700bd11e158d9
Eddie Hsu <aoe102198@gmail.com>
2024-10-06 21:33:22 +0800

    Add methods.funcs.go
    struct call normal method

---
+++ Lesson 5/methods-funcs.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+// Here's "Abs" written as a "regular function" with no change in functionality.
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(Abs(v)) // Abs() pass in Vertex
+}
