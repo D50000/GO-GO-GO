7cd9a5b41b75c8b04f1a803ce1787c24b16bb0e5
Eddie Hsu <aoe102198@gmail.com>
2024-10-10 21:21:27 +0800

    Complete the instructions

--- Lesson 5/indirection.go
+++ Lesson 5/indirection.go
@@ -6,6 +6,8 @@ type Vertex struct {
 	X, Y float64
 }
 
+// That is, as a convenience, Go interprets the statement v.Scale(5) as (&v).Scale(5)
+// since the Scale method has a pointer receiver.
 func (v *Vertex) Scale(f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
@@ -18,11 +20,14 @@ func ScaleFunc(v *Vertex, f float64) {
 
 func main() {
 	v := Vertex{3, 4}
-	v.Scale(2)
-	ScaleFunc(&v, 10)
+	v.Scale(2) // Whatever use "&v" or not it's OK
+	ScaleFunc(&v, 10) // no "&v" will compile error.
+
+	// For the statement v.Scale(5), even though v is a value and not a pointer,
+	// the method with the pointer receiver is called "automatically".
 
 	p := &Vertex{4, 3}
-	p.Scale(3)
+	p.Scale(3) // OK
 	ScaleFunc(p, 8)
 
 	fmt.Println(v, p)
