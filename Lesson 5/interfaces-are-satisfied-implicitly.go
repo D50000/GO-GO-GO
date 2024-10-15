616aac1eed744ebbce656fb0f01e28a4be8a9da7
Eddie Hsu <aoe102198@gmail.com>
2024-10-15 21:36:06 +0800

    Add interfaces-are-satisfied-implicitly.go
    Glang "interface" syntax

---
+++ Lesson 5/interfaces-are-satisfied-implicitly.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// "Interface" no need to instantly implement.
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+// This method means type T implements the interface I,
+// but we don't need to explicitly declare that it does so.
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+func main() {
+	var i I = T{"hello"}
+	i.M()
+}
