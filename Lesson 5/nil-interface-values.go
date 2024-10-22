48315fb5d751ecde7ecdb9d88730087d7d751b18
Eddie Hsu <aoe102198@gmail.com>
2024-10-22 20:42:01 +0800

    Update nil-interface-values.go
    - Fix syntax error
    - Add comments

--- Lesson 5/nil-interface-values.go
+++ Lesson 5/nil-interface-values.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 type I interface {
+	// A nil interface value holds neither value nor concrete type.
 	M()
 }
 
 func main() {
-	var i i
+	var i I
 	describe(i)
-	i.M()
+	i.M() // nil interface is a run-time error because there is no type inside the interface tuple to indicate which concrete method to call.
 }
 
 func describe(i I) {
