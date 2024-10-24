ab2961e298182efcc11820f7347e6d0371bd62f8
Eddie Hsu <aoe102198@gmail.com>
2024-10-25 00:10:35 +0800

    Complete instructions

--- Lesson 5/type-assertions.go
+++ Lesson 5/type-assertions.go
@@ -3,17 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var i interface{} = "hello"
+	var i interface{} = "Hello"
 
+	// Type Assertion
 	S := i.(string)
-	fmt.Println(s)
+	fmt.Println(s) // Hello
 
-	s, ok := i.(string)
-	fmt.Println(s, ok)
+	// This kind of assertion won't compile error when assert fail.
+	s, ok := i.(string) // assertion success
+	fmt.Println(s, ok) // Hello true
 
-	f, ok := i.(float64)
-	fmt.Println(f, ok)
+	f, ok := i.(float64) // assertion fail
+	fmt.Println(f, ok) // 0 false
 
-	f = i.(float64)
-	fmt.Println(f)
+	f = i.(float64) // compile error
+	fmt.Println(f) // panic: interface conversion: interface {} is string, not float64
 }
