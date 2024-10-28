4d806b1230ae685de30c7eb8b438e3eb989853cd
Eddie Hsu <aoe102198@gmail.com>
2024-10-28 21:15:10 +0800

    Complete the instructions

--- Lesson 5/type-switches.go
+++ Lesson 5/type-switches.go
@@ -5,11 +5,11 @@ import "fmt"
 func do(i interface{}) {
 	switch v := i.(type) {
 	case int:
-		fmt.Printf("Twice %v is %v\n", v, v*2)
+		fmt.Printf("Twice %v is %v\n", v, v*2) // %v prints the "value" 
 	case string:
-		fmt.Printf("%q is %v bytes long\n", v, len(v))
+		fmt.Printf("%q is %v bytes long\n", v, len(v)) // %q prints the "strings with double-quoted" 
 	default:
-		fmt.Printf("I don't know about type %T!\n", v)
+		fmt.Printf("I don't know about type %T!\n", v) // %T prints the "type" 
 	}
 }
 
