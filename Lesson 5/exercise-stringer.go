2f02d20f225550dad7d6ab04a1da593c9b502681
Eddie Hsu <aoe102198@gmail.com>
2024-11-02 01:45:12 +0800

    Update the conceptions

--- Lesson 5/exercise-stringer.go
+++ Lesson 5/exercise-stringer.go
@@ -4,16 +4,19 @@ import "fmt"
 
 type IPAddr [4]byte
 
-// TODO: Add a "String() string" method to IPAddr.
-// Add a "String() string" method to IPAddr.
+// Add a "String() string" method to IPAddr and overwrite the original method in "fmt package".
 func (ip IPAddr) String() string {
 	// Use `strings.Join` to join the individual byte parts with "."
 	// Convert each byte to a string representation.
-	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+	return fmt.Sprintf("\"%d.%d.%d.%d\"", ip[0], ip[1], ip[2], ip[3])
+	// "%d": print decimal integers.
+	// "%v": print generic placeholder for any value type.
 }
 
 func main(){
-	hosts := map[string]IPAddr{
+	// Hashmap: with string property with IPAddr value.
+	hosts := map[string]IPAddr{ 
+		// Initial
 		"loopback": {127, 0, 0, 1},
 		"googleDNS": {8, 8, 8, 8},
 	}
