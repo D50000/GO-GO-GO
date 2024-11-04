a82f1ed78bf56a936934ae9e1d02cbbc9fce7b28
Eddie Hsu <aoe102198@gmail.com>
2024-11-05 00:09:59 +0800

    Complete instructions

--- Lesson 5/errors.go
+++ Lesson 5/errors.go
@@ -10,18 +10,25 @@ type MyError struct {
 	What string
 }
 
+// Overwrite default "Error() method".
 func (e *MyError) Error() string {
 	return fmt.Sprintf("at %v, %s", e.When, e.What)
 }
 
+// The "error type" is a "built-in interface" similar to fmt.Stringer:
+//
+//	type error interface {
+//	    Error() string
+//	}
 func run() error {
-	return &MyError{
+	return &MyError{ // Initial the "MyError type" and return "non-nil error" type.
 		time.Now(),
 		"It don't work...",
 	}
 }
 
 func main() {
+	// A nil error denotes success; a non-nil error denotes failure.
 	if err := run(); err != nil {
 		fmt.Println(err)
 	}
