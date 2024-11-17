92513d0b23776e02bb312d8bd612951d4fd176a1
Eddie Hsu <aoe102198@gmail.com>
2024-11-18 00:43:34 +0800

    Complete the exercise-errors.go
    Important Concepts:
    - Implement the Error() interface and override the build-in one.
    - Avoid invoking the infinite Error() log.

--- Lesson 5/exercise-errors.go
+++ Lesson 5/exercise-errors.go
@@ -4,8 +4,29 @@ import (
 	"fmt"
 )
 
+// Error datatype.
+type ErrNegativeSqrt float64
+
+// "Implement the Error interface" and "override" the build-in one.
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("Cannot Sqrt negative number: %v", float64(e))
+	// Use "float64(e)" to prevent infinite error log.
+	// Root Cause:
+	// e ErrNegativeSqrt implement the Error() method will Sprintf(e) and check e type infinite.
+	// float64() is build-in simple type that doesn't implement Error() method.
+}
+
 func Sqrt(x float64) (float64, error) {
-	return 0, nil
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x) // Return {x, error}
+	}
+	
+	// Calculate the square root method and return nil error.
+	z := x
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z, nil
 }
 
 func main() {
