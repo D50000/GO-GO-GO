43be95a94921a5fd62fe04d45a2983ce59f23ff8
Eddie Hsu <aoe102198@gmail.com>
2024-11-21 00:04:37 +0800

    Update comments

--- Lesson 5/reader.go
+++ Lesson 5/reader.go
@@ -11,12 +11,12 @@ import (
 func main() {
 	r := strings.NewReader("Hello Reader!") // Similar with files class.
 
-	b := make([]byte, 8) // Initial 8 bytes data with [0, 0, 0, 0, 0, 0, 0, 0].
+	b := make([]byte, 8) // Initial 8 bytes data with [0 0 0 0 0 0 0 0].
 	for { // Infinity for loop until break.
 		n, err := r.Read(b)
-		fmt.Printf("n = %v err = %v b =%v\n", n, err, b)
-		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
+		fmt.Printf("n = %v err = %v b =%v\n", n, err, b) // n = 8 err = <nil> b =[72 101 108 108 111 32 82 101] b[:n] = "Hello Re"
+		fmt.Printf("b[:n] = %q\n", b[:n]) // n = 5 err = <nil> b =[97 100 101 114 33 32 82 101] b[:n] = "ader!"
+		if err == io.EOF { // n = 0 err = EOF b =[97 100 101 114 33 32 82 101] b[:n] = ""
 			break
 		}
 	}
