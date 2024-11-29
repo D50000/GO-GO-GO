9009274033148c3797b6ec07515adab77e0decfd
Eddie Hsu <aoe102198@gmail.com>
2024-11-30 01:03:39 +0800

    Add more detail comments

--- Lesson 5/images.go
+++ Lesson 5/images.go
@@ -13,7 +13,7 @@ import (
 // }
 
 func main() {
-	m := image.NewRGBA(image.Rect(0, 0, 100, 100)) // Draw Rectangle.
-	fmt.Println(m.Bounds())  // Show images boarder.
-	fmt.Println(m.At(0, 0).RGBA()) // Show the coordinate(0, 0) RGBA color.
+	m := image.NewRGBA(image.Rect(0, 0, 100, 100)) // Draw Rectangle => return Rectangle.
+	fmt.Println(m.Bounds())  // Show images boarder => return Rectangle.
+	fmt.Println(m.At(0, 0).RGBA()) // Show the coordinate(0, 0) RGBA color => return Color.
 }
