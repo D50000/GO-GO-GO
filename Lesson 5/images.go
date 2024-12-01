284132b65d0efdfbfe13a803887c4045dc6eccfe
Eddie Hsu <aoe102198@gmail.com>
2024-12-01 23:11:14 +0800

    Add detail of "image package" build-it functions

--- Lesson 5/images.go
+++ Lesson 5/images.go
@@ -5,7 +5,7 @@ import (
 	"image"
 )
 
-// Package "image" defines the Image interface:
+// Build-it image Package: "image" defines the Image interface:
 // type Image interface {
 //     ColorModel() color.Model
 //     Bounds() Rectangle
@@ -13,6 +13,8 @@ import (
 // }
 
 func main() {
+	// Build-it image Package: func NewRGBA(r Rectangle) *RGBA { }
+	// func Rect(x0, y0, x1, y1 int) Rectangle { }
 	m := image.NewRGBA(image.Rect(0, 0, 100, 100)) // Draw Rectangle => return Rectangle.
 	fmt.Println(m.Bounds())  // Show images boarder => return Rectangle.
 	fmt.Println(m.At(0, 0).RGBA()) // Show the coordinate(0, 0) RGBA color => return Color.
