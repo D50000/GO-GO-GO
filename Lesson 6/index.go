1a556b668520ddab75787b8e8c09b92dc31e51a2
Eddie Hsu <aoe102198@gmail.com>
2024-12-09 23:18:55 +0800

    Add "Generic Syntax"

--- Lesson 6/index.go
+++ Lesson 6/index.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 // Multiple types using "type parameters".
+// Generic Syntax:
+// "[T constraint]"
+// Interface example:
+// [T comparable] => support ==, != operator
+// [T any] => any type
+// Sets example:
+// [T ~int | ~float64] => type int or float64
 func Index[T comparable](s []T, x T) int {
 	for i, v := range s {
 		// v and x are type T, which has the comparable
