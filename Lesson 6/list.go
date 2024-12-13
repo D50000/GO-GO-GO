68ca04120e4ba9502453870a70e4867686f3fd00
Eddie Hsu <aoe102198@gmail.com>
2024-12-13 23:31:50 +0800

    Complete the list.go exercise

--- Lesson 6/list.go
+++ Lesson 6/list.go
@@ -1,11 +1,72 @@
 package main
 
-// List represents a singly-linked list that holds
-// values of any type.
+import (
+	"fmt"
+)
+
+// To make the "list functional", it will need:
+// - A method to append elements to the list.
+// - A method to traverse and print the elements.
+// - Additional helper functions as needed.
+
+// "List" represents a singly-linked list that holds values of any type.
 type List[T any] struct {
 	next *List[T]
 	val  T
 }
 
+// "Append" adds a new value to the end of the list.
+func (l *List[T]) Append(value T) {
+	current := l
+	// Traverse to the end of the list.
+	for current.next != nil {
+		current = current.next
+	}
+	// Add a new node with the given values.
+	current.next = &List[T]{val: value}
+}
+
+// "Traverse prints" all elements in the list.
+func (l *List[T]) Traverse() {
+	current := l
+	// Loop through the list and print values.
+	for current != nil {
+		fmt.Printf("%v : ", current.val)
+		current = current.next
+	}
+	fmt.Println("nil")
+}
+
+// "Length" returns the number of elements in the list.
+func (l *List[T]) Length() int {
+	count := 0
+	current := l
+	// Loop through the list and count nodes.
+	for current != nil {
+		count++
+		current = current.next
+	}
+	return count
+}
+
+// "Create" a new linked list with an initial value.
+func NewList[T any](value T) *List[T] {
+	return &List[T]{val: value}
+}
+
 func main() {
+	// Create a new list.
+	list := NewList(10)
+
+	// Append elements.
+	list.Append(20)
+	list.Append(30)
+	list.Append(40)
+
+	// Traverse and print.
+	fmt.Println("List elements:")
+	list.Traverse()
+
+	// Length of list.
+	fmt.Printf("Length of the list: %d\n", list.Length())
 }
