f5b4645185955783710e00f85bd132787de5983b
Eddie Hsu <aoe102198@gmail.com>
2024-12-25 23:50:43 +0800

    Add goroutines.go
    Goroutines: Concurrent, erupt simultaneously.

---
+++ Lesson 7/goroutines.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Microsecond)
+		fmt.Print(s)
+	}
+}
+
+func main() {
+	// A goroutine is a lightweight thread managed by the Go runtime.
+	// Goroutines run in the same address space, so access to shared memory must be synchronized.
+	go say("world") // Concurrent, erupt simultaneously.
+	say("hello")
+}
